0f46aa907cee0371ffdc507f7ced484e269470bb
Kaila Aquino <kailad.aquino@gmail.com>
2025-05-06 19:59:36 -0700

    Lab 8: Group Card component

---
+++ packages/proto/src/group-card.ts
@@ -0,0 +1,43 @@
+import { html, css, LitElement } from "lit";
+import { property } from "lit/decorators.js";
+import reset from "./styles/reset.css.ts";
+
+export class GroupCard extends LitElement {
+  @property() href = "";
+  @property() label = "";
+
+  override render() {
+    return html`
+      <div class="group-container">
+        <a href=${this.href} class="group-title">
+          <slot>${this.label}</slot>
+        </a>
+      </div>
+    `;
+  }
+
+  static styles = [
+    reset.styles,
+    css`
+      .group-container {
+        width: 300px;
+        height: 300px;
+        background-color: var(--color-bg-nav);
+        display: flex;
+        align-items: center; /* vertical center */
+        justify-content: center; /* horizontal center */
+        overflow: hidden;
+        border-radius: var(--radius-sm);
+        padding: var(--space-sm);
+      }
+
+      .group-title {
+        font-family: var(--font-body);
+        font-size: var(--size-font-xl);
+        overflow: hidden;
+        text-align: center;
+        color: var(--color-text-primary);
+      }
+    `,
+  ];
+}
