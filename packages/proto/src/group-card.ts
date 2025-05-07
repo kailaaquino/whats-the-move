10343935fb25cb553f90c45e1524152b9a5dfc8a
Kaila Aquino <kailad.aquino@gmail.com>
2025-05-07 15:48:37 -0700

    Lab 8: move html files out to proto

--- packages/proto/src/group-card.ts
+++ packages/proto/src/group-card.ts
@@ -2,41 +2,86 @@ import { html, css, LitElement } from "lit";
 import { property } from "lit/decorators.js";
 import reset from "./styles/reset.css.ts";
 
-/* GroupCard components are used within friend groups to represent actions to take */
 export class GroupCard extends LitElement {
+  // add properties
+  //   @property() color = "var(--color-accent-pink)";
+  @property() groupName = "";
+  @property() members = "";
+  @property() upcoming = "";
   @property() href = "";
-  @property() label = "";
+  @property({ attribute: "plans-href" }) plansHref = "";
+  @property({ attribute: "members-href" }) membersHref = "";
 
   override render() {
-    return html`
-      <div class="group-container">
-        <a href=${this.href} class="group-title">
-          <slot>${this.label}</slot>
+    return html`<div class="card-container">
+        <a href=${this.href} class="card-colorbox">
+        <div class="card-colorbox"></div>
+      </a>
+
+      <div class="card-text-container">
+        <a href=${this.href} class="card-textLg">
+          <div class="card-textLg">${this.groupName}</div>
+        </a>
+        <a href=${this.membersHref}>
+          <div class="card-textSm">Members: ${this.members}</div>
+        </a>
+        <div class="card-textSm">Upcoming: ${this.upcoming}</div>
+        <a href=${this.plansHref}>
+          <div class="card-textXs">View plans</div>
         </a>
       </div>
-    `;
+    </div>`;
   }
 
   static styles = [
     reset.styles,
     css`
-      .group-container {
-        width: 300px;
-        height: 300px;
+      .card-container {
+        width: 300px; /* fixed width so 3 fit in one row */
+        height: 300px; /* fixed height */
         background-color: var(--color-bg-nav);
         display: flex;
-        align-items: center; /* vertical center */
-        justify-content: center; /* horizontal center */
+        flex-direction: column;
+        gap: var(--space-xs);
+        flex-shrink: 0; /* prevent shrinking */
         overflow: hidden;
         border-radius: var(--radius-sm);
-        padding: var(--space-sm);
+      }
+      .card-colorbox {
+        width: 100%;
+        height: 50%; /* half of the parent height */
+        background-color: var(--color-accent-pink);
+      }
+      .card-text-container {
+        display: flex;
+        flex-direction: column;
+        gap: var(--space-xs);
+        width: 100%;
+        height: 50%;
+        padding: var(--space-xs);
+        overflow: hidden;
       }
 
-      .group-title {
+      .card-textLg {
         font-family: var(--font-body);
         font-size: var(--size-font-xl);
-        overflow: hidden;
-        text-align: center;
+        font-weight: bold;
+        color: var(--color-text-primary);
+      }
+      .card-textMd {
+        font-family: var(--font-body);
+        font-size: var(--size-font-lg);
+        color: var(--color-text-primary);
+      }
+      .card-textSm {
+        font-family: var(--font-body);
+        font-size: var(--size-font-small);
+        color: var(--color-text-primary);
+      }
+
+      .card-textXs {
+        font-family: var(--font-body);
+        font-size: var(--size-font-xs);
         color: var(--color-text-primary);
       }
     `,
